41b4bf8caa0c2d02a3516e03dd882c601dc96157
sarthak <sarthak@enablero.com>
2025-07-13 18:08:28 +0530

    Added service-down approach

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 const cors = require("cors");
 const app = express();
 
@@ -7,7 +8,7 @@ app.use(bodyParser.json()); // user sends json in body, it get parsed easily
 app.use(cors());
 
 const posts = {};
-// Posts structure 
+// Posts structure
 // posts ==== {
 //     id:'2132',
 //     title:'sasasa',
@@ -15,11 +16,8 @@ const posts = {};
 //         {id:'dsds',content:'dsd'}
 //     ]
 // }
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-app.post("/events", async (req, res) => {
-  const { type, data } = req.body;
+
+const handleEvent = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = {
@@ -33,22 +31,39 @@ app.post("/events", async (req, res) => {
     post.comments.push({
       id,
       content,
-      status
+      status,
     });
-  } else if(type==="CommentUpdated"){
-     const { id, postId, content, status } = data;
-     const post = posts[postId];
-     const comment = post.comments.find(comment=>{
-      return comment.id===id;
-     })
-     comment.status=status;
-     comment.content=content;
+  } else if (type === "CommentUpdated") {
+    const { id, postId, content, status } = data;
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+    comment.content = content;
   }
-
+};
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data);
   console.log(posts);
   res.send({});
 });
 
-app.listen(4002, () => {
-  console.log("Listening on 4002");
+app.listen(4002, async () => {
+ console.log("Listening on 4002");
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+ 
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
+ 
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
